e95aad69ddf43e2a695f9d1c130e4fac98e58aa9
softboysxp <softboysxp@gmail.com>
2016-02-21 15:58:38 -0800

    Adding expressjs route to handle sending MMS

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
+    nodemailer = require('nodemailer'),
+    fs = require('fs'),
     app = express();
 
+var mailerConfig = JSON.parse(fs.readFileSync('mailer-config.json'));
+
+var transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: mailerConfig.user,
+    pass: mailerConfig.pass
+  }
+});
+
 app.use(express.static(__dirname + '/html'));
+app.use(bodyParser.json({limit: '10mb'}));
+
+app.post('/sendMms', function (req, res) {
+  //console.log(req.body);
+
+  if (!(req.body && req.body.carrier && req.body.number)) {
+    res.status(500).send('Please specify both carrier and phone number');
+    return;
+  }
+
+  var mail = {
+    from: mailerConfig.from,
+    to: req.body.number + req.body.carrier,
+    subject: 'New Space Cam Snapshot',
+    text: 'You have a new Space Cam snapshot.',
+    attachments: [
+      { path: req.body.dataUrl }
+    ]
+  };
+
+  transporter.sendMail(mail, function(err, info){
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    console.log('Message sent: ' + info.response);
+    res.send('OK');
+  });
+});
+
 app.listen(8080, '0.0.0.0');
--- html/js/dist/all.js
+++ html/js/dist/all.js
@@ -1,3 +1,171 @@
+(function() {
+  'use strict';
+
+  var DROPBOX_CLIENT_KEY = '1m07w66d6okvwry',
+      SEND_MMS_ENDPOINT = 'http://localhost:8080/sendMms',
+      PIXEL_INTENSITY_CHANGE_THRESHOLD = 0.2,
+      FRAME_INTENSITY_CHANGE_THRESHOLD = 0.2;
+
+  var app = angular.module('app', ['ngStorage']);
+
+  app.controller('controller', function ($scope, $localStorage, $sessionStorage, $http) {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    $scope.getUserMediaSupported = !!navigator.getUserMedia;
+
+    if (!$scope.getUserMediaSupported) {
+      return;
+    }
+
+    $scope.localMediaStream = null;
+    $scope.settings = $localStorage;
+    $scope.settings.snapshotActions = $scope.settings.snapshotActions || { saveToDropbox: false, sendMms: false };
+
+    var linkDropbox = function() {
+      if ($scope.dropbox) {
+        return;
+      }
+
+      (new Dropbox.Client({ key: DROPBOX_CLIENT_KEY })).authenticate(function(err, client) {
+        if (err) {
+          console.log(err);
+          $scope.errorMessage = err;
+          return;
+        }
+
+        client.getAccountInfo(function(err, accountInfo) {
+          if (err) {
+            console.log(err);
+            $scope.errorMessage = err;
+            return;
+          }
+
+          $scope.$apply(function() {
+            $scope.dropboxAccountInfo = accountInfo;
+          });
+        });
+
+        $scope.dropbox = client;
+      });
+    };
+
+    $scope.unlinkDropbox = function() {
+      if (!$scope.dropbox) {
+        return;
+      }
+
+      $scope.dropbox.signOut(function() {
+        $scope.$apply(function() {
+          delete $scope.dropbox;
+          $scope.settings.snapshotActions.saveToDropbox = false;
+        });
+      });
+    };
+
+    var saveToDropbox = function(imageBlob) {
+      var filename = (new Date()).toISOString() + '.png';
+      $scope.dropbox.writeFile(filename, imageBlob, function(err, stat) {
+        if (err) {
+          $scope.errorMessage = err;
+          return;
+        }
+
+        console.log(stat);
+      });
+    };
+
+    $scope.$watch('settings.snapshotActions.saveToDropbox', function(newValue, oldValue) {
+      if (newValue && !$scope.dropbox) {
+        linkDropbox();
+      }
+    });
+
+    var sendMms = function(dataUrl) {
+      if (!$scope.settings.carrier || !$scope.settings.number) {
+        return;
+      }
+
+      $http.post(
+        SEND_MMS_ENDPOINT,
+        {
+          carrier: $scope.settings.carrier,
+          number: $scope.settings.number,
+          dataUrl: dataUrl
+        }
+      ).then(function(response) {
+
+      }, function(err) {
+        $scope.errorMessage = 'Error sending MMS: ' + err.status;
+      });
+    };
+
+    var video = $('video#cam')[0],
+        buffer = $('canvas#buffer')[0],
+        ctx = $('canvas#buffer')[0].getContext('2d'),
+        currentSampleFrame = null,
+        previousSampleFrame = null,
+        sampleRate = 60, // sample rate in frames
+        frameSinceLastSample = 0;
+
+    var detectFrameChange = function(frame1, frame2) {
+      var diff = 0;
+      for (var i = 0; i < frame1.length; i += 4) {
+        var r1 = frame1[i], g1 = frame1[i + 1], b1 = frame1[i + 2];
+        var r2 = frame2[i], g2 = frame2[i + 1], b2 = frame2[i + 2];
+        var i1 = 0.2989 * r1 + 0.5870 * g1 + 0.1140 * b1;
+        var i2 = 0.2989 * r2 + 0.5870 * g2 + 0.1140 * b2;
+        diff += Math.abs(i1 - i2) / 255 > PIXEL_INTENSITY_CHANGE_THRESHOLD;
+      }
+
+      return diff / (frame1.length / 4) > FRAME_INTENSITY_CHANGE_THRESHOLD;
+    };
+
+    var render = function() {
+      ctx.drawImage(video, 0, 0);
+
+      if (frameSinceLastSample++ > sampleRate) {
+        previousSampleFrame = currentSampleFrame;
+        currentSampleFrame = ctx.getImageData(0, 0, 640, 480).data;
+
+        if (previousSampleFrame && currentSampleFrame && detectFrameChange(previousSampleFrame, currentSampleFrame)) {
+          console.log('frame change detected');
+          if ($scope.settings.autosave) {
+            $scope.snapshot();
+          }
+        }
+
+        frameSinceLastSample = 0;
+      }
+
+      requestAnimationFrame(render);
+    };
+
+    $scope.snapshot = function() {
+      if ($scope.settings.snapshotActions.saveToDropbox) {
+        buffer.toBlob(function(blob) {
+          saveToDropbox(blob);
+        });
+      }
+
+      if ($scope.settings.snapshotActions.sendMms) {
+        sendMms(buffer.toDataURL());
+      }
+    };
+
+    navigator.getUserMedia({
+        audio: false,
+        video: true
+      },
+      function(stream) {
+        $scope.localMediaStream = stream;
+        $('video#cam').attr('src', window.URL.createObjectURL($scope.localMediaStream));
+        requestAnimationFrame(render);
+      },
+      function(err) {
+        $scope.errorMessage = 'Error getting user media: ' + err;
+      }
+    );
+  });
+})();
 !function(t){"use strict";var e=t.HTMLCanvasElement&&t.HTMLCanvasElement.prototype,o=t.Blob&&function(){try{return Boolean(new Blob)}catch(t){return!1}}(),n=o&&t.Uint8Array&&function(){try{return 100===new Blob([new Uint8Array(100)]).size}catch(t){return!1}}(),r=t.BlobBuilder||t.WebKitBlobBuilder||t.MozBlobBuilder||t.MSBlobBuilder,a=/^data:((.*?)(;charset=.*?)?)(;base64)?,/,i=(o||r)&&t.atob&&t.ArrayBuffer&&t.Uint8Array&&function(t){var e,i,l,u,b,c,d,B,f;if(e=t.match(a),!e)throw new Error("invalid data URI");for(i=e[2]?e[1]:"text/plain"+(e[3]||";charset=US-ASCII"),l=!!e[4],u=t.slice(e[0].length),b=l?atob(u):decodeURIComponent(u),c=new ArrayBuffer(b.length),d=new Uint8Array(c),B=0;B<b.length;B+=1)d[B]=b.charCodeAt(B);return o?new Blob([n?d:c],{type:i}):(f=new r,f.append(c),f.getBlob(i))};t.HTMLCanvasElement&&!e.toBlob&&(e.mozGetAsFile?e.toBlob=function(t,o,n){t(n&&e.toDataURL&&i?i(this.toDataURL(o,n)):this.mozGetAsFile("blob",o))}:e.toDataURL&&i&&(e.toBlob=function(t,e,o){t(i(this.toDataURL(e,o)))})),"function"==typeof define&&define.amd?define(function(){return i}):"object"==typeof module&&module.exports?module.exports=i:t.dataURLtoBlob=i}(window);
 //# sourceMappingURL=canvas-to-blob.min.js.map
 'use strict';
@@ -145,171 +313,6 @@ $(window).on({
   }
 });
 
-
-
-;(function() {
-  'use strict';
-
-  var DROPBOX_CLIENT_KEY = '1m07w66d6okvwry',
-    SEND_MMS_ENDPOINT = 'http://localhost:4000/sendMms',
-        PIXEL_INTENSITY_CHANGE_THRESHOLD = 0.2,
-        FRAME_INTENSITY_CHANGE_THRESHOLD = 0.2;
-
-  var app = angular.module('app', ['ngStorage']);
-
-  app.controller('controller', function ($scope, $localStorage, $sessionStorage, $http) {
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    $scope.getUserMediaSupported = !!navigator.getUserMedia;
-
-    if (!$scope.getUserMediaSupported) {
-      return;
-    }
-
-    $scope.localMediaStream = null;
-    $scope.settings = $localStorage;
-    $scope.settings.snapshotActions = $scope.settings.snapshotActions || { saveToDropbox: false, sendMms: false };
-
-    var linkDropbox = function() {
-      if ($scope.dropbox) {
-        return;
-      }
-
-      (new Dropbox.Client({ key: DROPBOX_CLIENT_KEY })).authenticate(function(err, client) {
-        if (err) {
-          console.log(err);
-          $scope.errorMessage = err;
-          return;
-        }
-
-        client.getAccountInfo(function(err, accountInfo) {
-          if (err) {
-            console.log(err);
-            $scope.errorMessage = err;
-            return;
-          }
-
-          $scope.$apply(function() {
-            $scope.dropboxAccountInfo = accountInfo;
-          });
-        });
-
-        $scope.dropbox = client;
-      });
-    };
-
-    $scope.unlinkDropbox = function() {
-      if (!$scope.dropbox) {
-        return;
-      }
-
-      $scope.dropbox.signOut(function() {
-        $scope.$apply(function() {
-          delete $scope.dropbox;
-          $scope.settings.snapshotActions.saveToDropbox = false;
-        });
-      });
-    };
-
-    var saveToDropbox = function(imageBlob) {
-      var filename = (new Date()).toISOString() + '.png';
-      $scope.dropbox.writeFile(filename, imageBlob, function(err, stat) {
-        if (err) {
-          $scope.errorMessage = err;
-          return;
-        }
-
-        console.log(stat);
-      });
-    };
-
-    $scope.$watch('settings.snapshotActions.saveToDropbox', function(newValue, oldValue) {
-      if (newValue && !$scope.dropbox) {
-        linkDropbox();
-      }
-    });
-
-    var sendMms = function() {
-      $http.post(
-        SEND_MMS_ENDPOINT,
-        {
-          carrier: '',
-          number: ''
-        }
-      ).then(function(response) {
-
-      }, function(err) {
-        $scope.errorMessage = 'Error sending MMS: ' + err.status;
-      });
-    };
-
-    var video = $('video#cam')[0],
-        buffer = $('canvas#buffer')[0],
-        ctx = $('canvas#buffer')[0].getContext('2d'),
-        currentSampleFrame = null,
-        previousSampleFrame = null,
-        sampleRate = 60, // sample rate in frames
-        frameSinceLastSample = 0;
-
-    var detectFrameChange = function(frame1, frame2) {
-      var diff = 0;
-      for (var i = 0; i < frame1.length; i += 4) {
-        var r1 = frame1[i], g1 = frame1[i + 1], b1 = frame1[i + 2];
-        var r2 = frame2[i], g2 = frame2[i + 1], b2 = frame2[i + 2];
-        var i1 = 0.2989 * r1 + 0.5870 * g1 + 0.1140 * b1;
-        var i2 = 0.2989 * r2 + 0.5870 * g2 + 0.1140 * b2;
-        diff += Math.abs(i1 - i2) / 255 > PIXEL_INTENSITY_CHANGE_THRESHOLD;
-      }
-
-      return diff / (frame1.length / 4) > FRAME_INTENSITY_CHANGE_THRESHOLD;
-    };
-
-    var render = function() {
-      ctx.drawImage(video, 0, 0);
-
-      if (frameSinceLastSample++ > sampleRate) {
-        previousSampleFrame = currentSampleFrame;
-        currentSampleFrame = ctx.getImageData(0, 0, 640, 480).data;
-
-        if (previousSampleFrame && currentSampleFrame && detectFrameChange(previousSampleFrame, currentSampleFrame)) {
-          console.log('frame change detected');
-          if ($scope.settings.autosave) {
-            $scope.snapshot();
-          }
-        }
-
-        frameSinceLastSample = 0;
-      }
-
-      requestAnimationFrame(render);
-    };
-
-    $scope.snapshot = function() {
-      buffer.toBlob(function(blob) {
-        if ($scope.settings.snapshotActions.saveToDropbox) {
-          saveToDropbox(blob);
-        }
-
-        if ($scope.settings.snapshotActions.sendMms) {
-          sendMms(blob);
-        }
-      });
-    };
-
-    navigator.getUserMedia({
-        audio: false,
-        video: true
-      },
-      function(stream) {
-        $scope.localMediaStream = stream;
-        $('video#cam').attr('src', window.URL.createObjectURL($scope.localMediaStream));
-        requestAnimationFrame(render);
-      },
-      function(err) {
-        $scope.errorMessage = 'Error getting user media: ' + err;
-      }
-    );
-  });
-})();
 /**
  * Owl carousel
  * @version 2.0.0
